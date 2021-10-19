c4560e86f99eee9339daf4d4ab948b9229054197
Raquel Boriero <raquelboriero@hotmail.com>
2021-10-19 17:17:29 +0100

    .

--- js/main.js
+++ js/main.js
@@ -3,9 +3,10 @@ function calcularJurosSimples() {
     var capital = (document.getElementById("fcapital")).value;
     var taxa = (document.getElementById("ftaxa")).value;
     var periodo = (document.getElementById("fperiodo")).value;
+    var juros = capital * taxa * periodo;
 
-    console.log(capital, taxa, periodo);
-  //  var capital = elementoCapital.value;
-  //  console.log(capital);
+    console.log(juros);
+  //  console.log(capital, taxa, periodo);
+ 
 
 }
