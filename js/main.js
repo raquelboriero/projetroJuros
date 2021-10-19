c23c57784028826150308bd6fbbe1fd519a81320
Raquel Boriero <raquelboriero@hotmail.com>
2021-10-19 17:14:11 +0100

    .

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+function calcularJurosSimples() {
+    var capital = (document.getElementById("fcapital")).value;
+    var taxa = (document.getElementById("ftaxa")).value;
+    var periodo = (document.getElementById("fperiodo")).value;
+
+    console.log(capital, taxa, periodo);
+  //  var capital = elementoCapital.value;
+  //  console.log(capital);
+
+}
