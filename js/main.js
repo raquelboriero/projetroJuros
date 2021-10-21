17379086d734ebabf572190ecf6b91a90061d8dc
Raquel Boriero <raquelboriero@hotmail.com>
2021-10-21 17:22:27 +0100

    .

--- js/main.js
+++ js/main.js
@@ -4,9 +4,30 @@ function calcularJurosSimples() {
     var taxa = (document.getElementById("ftaxa")).value;
     var periodo = (document.getElementById("fperiodo")).value;
     var juros = capital * taxa * periodo;
+    var vTotal = parseFloat(capital) + parseFloat(juros);
+    var resultJSimples = document.getElementById("resultJSimples");
+    resultJSimples.innerHTML = "O valor do juros será de R$" + juros + "! Ao final de " + periodo + " anos, você terá R$" + vTotal + ".";
+  console.log(vTotal);
 
-    console.log(juros);
-  //  console.log(capital, taxa, periodo);
+  //  validar os 3 campos 
+
+}
+
+function calcularJurosComposto() {
+  var capital = (document.getElementById("fcapital")).value;
+  var taxa = (document.getElementById("ftaxa")).value;
+  var periodo = (document.getElementById("fperiodo")).value;
+  var base = parseFloat(1 + taxa);
+  var montanteAPagar = Math.pow(base, periodo);
+  console.log(montanteAPagar);
  
+  
+
+
+  // var resultJComposto = document.getElementById("resultJComposto");
+  // resultJComposto.innerHTML = "O valor do juros será de R$" + MontanteAPagar + "! Ao final de " + periodo + " anos, você terá R$" + vTotal + ".";
+
+
+//  validar os 3 campos 
 
 }
